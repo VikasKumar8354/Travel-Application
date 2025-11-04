118caf29d11fe9a95c819f18aa7300c7e231ebf3
VikasKumar8354 <120902vikaskumar@gmail.com>
2025-11-04 17:16:36 +0530

    updates

--- TravelApplication/src/main/java/com/travel/TravelApplication/Model/User.java
+++ TravelApplication/src/main/java/com/travel/TravelApplication/Model/User.java
@@ -13,7 +13,6 @@ public class User {
 
     private String name;
     private String email;
-    private String password;
     private String phone;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
@@ -61,14 +60,6 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getPhone() {
